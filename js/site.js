1e33397e77a4557d04ac055e4f0b897ea6d00ad4
Ceoael <52048657+Ceoael@users.noreply.github.com>
2021-08-03 12:37:30 +0200

    Finished App
    Finished logic, added description

--- js/site.js
+++ js/site.js
@@ -1,6 +1,43 @@
-//Call Hello World
-function helloWorld() {
-    console.log('Hello World!');
+function getValues() {
+    let startValue = document.getElementById('startValue').value;
+    let endValue = document.getElementById('endValue').value;
+
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+    
+    if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+        const numbers = generateNumbers(startValue, endValue);
+        displayNumbers(numbers);
+    } else {
+        alert("You must enter integers");
+    }
 }
 
-helloWorld();
+function generateNumbers(startValue, endValue) {
+    const numbers = [];
+
+    for(let number = startValue; number <= endValue; number++) {
+        numbers.push(number);
+    }
+
+    return numbers;
+}
+
+function displayNumbers(numbers) {
+    let templateRows = "";
+
+    for (let index = 0; index < numbers.length; index++) {
+        let className = "even";
+        const number = numbers[index];
+
+        if(number % 2 === 0) {
+            className = "even";
+        } else {
+            className = "odd";
+        }
+
+        templateRows += `<tr><td class="${className}">${number}</td></tr>`;
+    }
+
+    document.getElementById("results").innerHTML = templateRows;
+}
